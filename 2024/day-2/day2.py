b9cffcf492b364546672b015b8b92750f0e6b1d6
ellis-burgess <ellis.burgess@bjss.com>
2024-12-02 09:09:57 +0000

    day 2 solution

--- 2024/day-2/day2.py
+++ 2024/day-2/day2.py
@@ -5,18 +5,27 @@ with open('input.txt') as f:
     report = [int(i) for i in line.strip().split()]
     reports.append(report)
 
-print(reports[0])
-
-count = 0
-
-for report in reports:
+def is_report_safe(report):
   if report != sorted(report, reverse=False) and report != sorted(report, reverse=True):
-    continue
-  elif len(set(report)) != len(report):
-    continue
+    return False
+  if len(set(report)) != len(report):
+    return False
   else:
     report_delta = [abs(report[i] - report[max(i - 1, 0)]) for i in range(len(report))]
     if all([i > 0 and i < 4 for i in report_delta[1:]]):
-      count += 1
+      return True
+
+count = 0
+tolerated_count = 0
+
+for report in reports:
+  if is_report_safe(report):
+    count += 1
+    tolerated_count += 1
+  else:
+    masked_reports = [report[0:i] + report[i+1:] for i in range(len(report))]
+    if any([is_report_safe(masked_report) for masked_report in masked_reports]):
+      tolerated_count += 1
 
 print(count)
+print(tolerated_count)
